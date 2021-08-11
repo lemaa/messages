c0d89708224b6d7ffc661a7a7075e098a0c95415
amel <amel@3dosft.fr>
2021-08-11 22:41:42 +0200

    fix(api): change api end point

--- src/actions/messageActions.ts
+++ src/actions/messageActions.ts
@@ -26,9 +26,8 @@ export const fetchMessageDispatch = (messages: Message[]) =>
 
 export const createMessage = async (message: Message, dispatch: React.Dispatch<MessageAction>) => {
         try {
-            const response = await axios.post(`${MESSAGE_ENDPOINT}/create`, message)
-                ;
-            return dispatch(createMessageDispatch(response.data.message));
+            //const response = await axios.post(`${MESSAGE_ENDPOINT}/create`, message);
+            return dispatch(createMessageDispatch(message));
         }
         catch (error) {
             throw (error);
@@ -38,7 +37,7 @@ export const createMessage = async (message: Message, dispatch: React.Dispatch<M
 
 export const fetchAllMessages =  async (dispatch: any) => {
     try {
-        const response = await axios.get(`${MESSAGE_ENDPOINT}/messages`);
+        const response = await axios.get(`${MESSAGE_ENDPOINT}`);
 
             return dispatch(fetchMessageDispatch(response.data));
         }
